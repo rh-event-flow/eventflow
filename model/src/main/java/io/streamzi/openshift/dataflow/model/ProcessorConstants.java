4f380027a3c5a4012e5f6b27c4afa1c34e1d0d8b
hhiden <hhiden@172.17.130.152>
2018-08-14 14:54:34 +0100

    More work on directly attaching to topics

--- model/src/main/java/io/streamzi/openshift/dataflow/model/ProcessorConstants.java
+++ model/src/main/java/io/streamzi/openshift/dataflow/model/ProcessorConstants.java
@@ -4,7 +4,12 @@ package io.streamzi.openshift.dataflow.model;
  * Various constants
  * @author hhiden
  */
-public class ProcessorConstants {
+public interface ProcessorConstants {
+    public enum ProcessorType {
+        DEPLOYABLE_IMAGE,
+        TOPIC_ENDPOINT
+    }
+    
     public static final String KAFKA_BOOTSTRAP_SERVERS = "STREAMZI_KAFKA_BOOTSTRAP_SERVER";
     public static final String NODE_UUID = "STREAMZI_NODE_UUID";
 }
