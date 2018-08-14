95e0cdb5f76b92daea8bee9a1dfe714ee2736590
Matthias Wessendorf <matzew@apache.org>
2018-08-14 12:13:45 +0200

    :lipstick: use correct ENV VARs

--- model/src/main/java/io/streamzi/openshift/dataflow/model/ProcessorConstants.java
+++ model/src/main/java/io/streamzi/openshift/dataflow/model/ProcessorConstants.java
@@ -5,6 +5,6 @@ package io.streamzi.openshift.dataflow.model;
  * @author hhiden
  */
 public class ProcessorConstants {
-    public static final String KAFKA_BOOTSTRAP_SERVERS = "STREAMZI_KAFKA_BOOTSTRAP_SERVER";
+    public static final String KAFKA_BOOTSTRAP_SERVERS = "BOOTSTRAP_SERVERS";
     public static final String NODE_UUID = "STREAMZI_NODE_UUID";
 }
